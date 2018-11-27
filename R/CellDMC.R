57c5d4d58e3f7b97e3bac1ddd46fafd3669c5ccf
Shijie C. Zheng <shijiezheng@gmail.com>
2018-11-27 19:40:49 +0800

    pre release

--- R/CellDMC.R
+++ R/CellDMC.R
@@ -13,7 +13,7 @@
 #' 
 #' @param beta.m
 #' A beta value matrix with rows labeling the CpGs and columns labeling 
-#' samples. This contains all CpGs, from which you want to find DMCTs.
+#' samples. 
 #' 
 #' @param pheno.v
 #' A vector of phenotype. CellDMC can handle both of binary and 
@@ -23,17 +23,17 @@
 #' @param frac.m
 #' A matrix contains fractions of each cell-type. Each row labels a sample, 
 #' with the same order of the columns in beta.m. Each column labels a 
-#' cell-type. Column names, which are names of cell-types, are required. The 
+#' cell-type. Column names, which are the names of cell-types, are required. The 
 #' rowSums of frac.m should be 1 or close to 1.
 #' 
 #' @param adjPMethod
-#' The method will be ued to adjust p values. The method can be any of method
+#' The method will be used to adjust p values. The method can be any of method
 #'  accepted by \code{\link{p.adjust}}.
 #' 
 #' @param adjPThresh
 #' A numeric value, default as 0.05. This is used to call significant DMCTs. 
 #' Adjusted p values less than this threshold will be reported as DMCTs (-1 or
-#'  1) in the first item 'dmct' of returned list.
+#'  1) in the 'dmct' matrix in the returned list.
 #' 
 #' @param cov.mod
 #' A design matrix from \code{model.matrix}, which contains other covariates to
@@ -42,9 +42,9 @@
 #'  cell-type fraction here!
 #' 
 #' @param sort
-#' Default as \code{FALSE}. If \code{TRUE}, the data.frame in coe list will 
-#' sorted based on p value of each CpG. The order of 'dmct' will not change 
-#' since the orders of each cell-type are different.
+#' Default as \code{FALSE}. If \code{TRUE}, the data.frame in coe list will be
+#' sorted based on p value of each CpG. The order of rows in 'dmct' will not 
+#' change since the orders of each cell-type are different.
 #' 
 #' @param mc.cores
 #' The number of cores to use, i.e. at most how many threads will 
@@ -53,10 +53,10 @@
 #' @return A list with the following two items. 
 #' 
 #' @return dmct
-#' A matrix tells wheter the input CpGs are DMCTs and DMCs. The first column 
+#' A matrix givess wheter the input CpGs are DMCTs and DMCs. The first column 
 #' gives whether a CpG is DMC or not. If the CpG is called as DMC, the value 
 #' will be 1, otherwise it is 0. The following columns give DMCTs for each 
-#' cell-types. If a CpG is DMCT, the value will be 1 (hypermethylated for case 
+#' cell-types. If a CpG is a DMCT, the value will be 1 (hypermethylated for case 
 #' compared to control) or -1 (hypomethylated for case compared to control). 
 #' Otherwise, the value is 0 (non-DMCT). The rows of this matrix are ordered as
 #' the same as input \code{beta.m}. 
@@ -82,7 +82,7 @@
 #' data(DummyBeta.m)
 #' out.l <- epidish(DummyBeta.m, centEpiFibIC.m, method = 'RPC')
 #' frac.m <- out.l$estF
-#' pheno.v <- factor(rep(c(0, 1), each = 5))
+#' pheno.v <- rep(c(0, 1), each = 5)
 #' celldmc.o <- CellDMC(DummyBeta.m, pheno.v, frac.m) 
 #' # Pls note this is a faked beta value matrix.
 #' 
