a60c286a4e8bf1d7a952cdf259a57b408c664827
Shijie C. Zheng <shijieczheng@gmail.com>
2022-10-11 15:38:10 +0800

    add 12 blood CT reference

---
+++ R/cent12CT450k.R
@@ -0,0 +1,36 @@
+#' Whole blood reference of 12 blood cell subtypes for 450k array
+#'
+#' This reference is constructed using data from Salas et al.(2022). It contains
+#'  the following 12 blood cell subtypes. This reference is constructed for 450k arrays.
+#'  See \code{cent12CT.m} for EPIC arrays.
+#'
+#' \itemize{
+#'   \item CD4+ naive T-cells
+#'   \item Basophil cells
+#'   \item CD4+ memory T-cells
+#'   \item Memory B-cells
+#'   \item Naive B-cells
+#'   \item Regulatory T-Cells
+#'   \item CD8+ memory T-cells
+#'   \item CD8+ naive T-cells
+#'   \item Eosinophils
+#'   \item NK-cells
+#'   \item Neutrophils 
+#'   \item Monocytes
+#' }
+#'
+#' @docType data
+#' @keywords datasets
+#' @name cent12CT450k.m
+#' @usage data(cent12CT450k.m)
+#' @format A matrix with 600 rows and 12 columns
+#' @references 
+#' Lucas A Salas, Ze Zhang, Devin C Koestler, Rondi A Butler, Helen M Hansen,
+#'  Annette M Molinaro, John K Wiencke, Karl T Kelsey, Brock C Christensen
+#' \emph{Enhanced cell deconvolution of peripheral blood using DNA methylation for high-resolution immune profiling.}
+#' Nat Commun. (2022) 13: 761.
+#' doi:\href{https://doi.org/10.1038/s41467-021-27864-7}{
+#' 10.1038/s41467-021-27864-7}.
+#' 
+#' 
+NULL
